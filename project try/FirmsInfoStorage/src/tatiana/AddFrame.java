5289c51b51a248d37cf9dd322e3b5e212692693f
TatianaGancheva <tatiana_g@abv.bg>
2015-09-08 19:25:49 +0300

    Project Lottery game

---
+++ project try/FirmsInfoStorage/src/tatiana/AddFrame.java
@@ -0,0 +1,49 @@
+package tatiana;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+
+public class AddFrame {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddFrame window = new AddFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AddFrame() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(103, 82, 2, 2);
+		frame.getContentPane().add(scrollPane);
+	}
+
+}
